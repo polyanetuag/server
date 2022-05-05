1d088189244e3d1ac61a0ebe25f913cb6d490046
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-05 19:29:23 -0300

    feat: listen port

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-const teste = 1;
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    return res.send('Hello World');
+})
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333');
+    }
+);
