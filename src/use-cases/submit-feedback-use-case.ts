b779e61b15da995d382713dda1bf4febf907fecc
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-06 20:14:16 -0300

    feat: submit feedbacks configuration

---
+++ src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,36 @@
+import { MailAdapter } from '../adapters/mail-adapter';
+import {FeedbacksRepository} from '../repositories/feedbacks-repository';
+
+//Regra de negócio da aplicação
+interface SubmitFeedbackUseCaseRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+  constructor(
+      private feedbacksRepository: FeedbacksRepository,
+      private mailAdapter: MailAdapter
+      ) {}
+
+  async execute(request: SubmitFeedbackUseCaseRequest) {
+    const { type, comment, screenshot } = request;
+    await this.feedbacksRepository.create({
+      type,
+      comment,
+      screenshot,
+    });
+
+    await this.mailAdapter.sendMail({
+      subject: 'Novo feedback',
+      body: [
+        `<div style='font-family: sans-serif; font-size: 16px; color: #111;'>`,
+        `<p>Novo feedback recebido!</p>`,
+        `<p>Tipo: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        `</div>`,
+      ].join("\n")
+    });
+  }
+}
