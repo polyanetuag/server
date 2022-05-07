498ca4a6e8f61b2409969beda135609b87119cf8
Polyane Tuag <pollyportugues@hotmail.com>
2022-05-07 17:08:42 -0300

    fix: install body-parser for error 413

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import { routes } from "./routes";
 
 const app = express();
+app.use(bodyParser.json({ limit: "100kb" }));
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
